dc52deb9c7b67090826aa23b76c73a37000479f2
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-03-12 13:37:33 +0530

    Update dl_button.py

--- plugins/dl_button.py
+++ plugins/dl_button.py
@@ -5,6 +5,8 @@ import logging
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.ERROR)
+
 import asyncio
 import aiohttp
 import os
