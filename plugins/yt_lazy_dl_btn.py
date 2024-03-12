145dbb5a4b3b486855c6b286a09bc352ca096c4b
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-03-12 13:39:56 +0530

    Update yt_lazy_dl_btn.py

--- plugins/yt_lazy_dl_btn.py
+++ plugins/yt_lazy_dl_btn.py
@@ -5,6 +5,7 @@ import logging
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.ERROR)
 import asyncio
 import json
 import os
